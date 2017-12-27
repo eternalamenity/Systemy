c8634804f69b9d970352d0eb492218ed9001b6b1
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2017-12-27 18:08:15 +0100

    Ver 2.0

--- Documents/NetBeansProjects/SOSOSOSOSOPY/src/sososososopy/Configuration.java
+++ Documents/NetBeansProjects/SOSOSOSOSOPY/src/sososososopy/Configuration.java
@@ -4,5 +4,5 @@ package sososososopy;
 public class Configuration 
 {
     public static final int DISC_CAPACITY = 32;
-    public static final int CLUSTER_CAPACITY = 256;
+    public static final int CLUSTER_CAPACITY = 32;
 }
